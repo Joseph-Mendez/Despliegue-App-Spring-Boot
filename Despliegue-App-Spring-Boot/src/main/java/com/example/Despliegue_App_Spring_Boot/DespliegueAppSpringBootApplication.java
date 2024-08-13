2c4092fa53d3f0fab9087d7fee9e78e94985ae9e
Joseph <josephalainmendez@gmail.com>
2024-08-13 10:11:54 -0600

    Primer commit

---
+++ Despliegue-App-Spring-Boot/src/main/java/com/example/Despliegue_App_Spring_Boot/DespliegueAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Despliegue_App_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespliegueAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespliegueAppSpringBootApplication.class, args);
+	}
+
+}
